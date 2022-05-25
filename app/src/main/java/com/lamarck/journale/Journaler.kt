64f5ad940e06938b2e4d59387d608a15db85decb
yuran-nhassengo <yurannhassengo@gmail.com>
2022-05-25 11:45:29 +0200

    add class jounerly and edit activity_main

---
+++ app/src/main/java/com/lamarck/journale/Journaler.kt
@@ -0,0 +1,16 @@
+package com.lamarck.journale
+
+import android.app.Application
+import android.content.Context
+
+class Journaler:Application() {
+
+    companion object{
+        var ctx: Context? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        ctx = applicationContext
+    }
+}
